2c5938b8454397903b545440e9e51554da97923b
Yosuke Onoue <onoue@likr-lab.com>
2019-09-29 12:24:52 +0900

    update

--- src/pages/root/index.js
+++ src/pages/root/index.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import team1Image from '../../images/team1.png'
+import team2Image from '../../images/team2.png'
+import team3Image from '../../images/team3.png'
+import team4Image from '../../images/team4.png'
+import team5Image from '../../images/team5.png'
+import team6Image from '../../images/team6.png'
+import team7Image from '../../images/team7.png'
+import team8Image from '../../images/team8.png'
 import judgitImage from '../../images/judgit.png'
 import housingImage from '../../images/housing.png'
 import egridImage from '../../images/egrid.png'
@@ -38,7 +46,7 @@ const RootPage = () => {
         <div className='hero-body'>
           <div className='container'>
             <h1 className='title has-text-centered'>
-              2019夏季オープンキャンパス
+              2019秋季オープンキャンパス
             </h1>
             <h2 className='subtitle has-text-centered'>
               日本大学文理学部情報科学科 尾上研究室
@@ -48,6 +56,29 @@ const RootPage = () => {
       </section>
       <section className='section'>
         <div className='columns is-multiline is-vcentered'>
+          <Card
+            image={team1Image}
+            url='https://codepen.io/tsubasa_0111/full/vqrwYd'
+          />
+          <Card
+            image={team2Image}
+            url='https://codepen.io/sawa38/full/KOzEoZ'
+          />
+          <Card
+            image={team3Image}
+            url='https://codepen.io/Arasou-0206/full/ZgEbyO'
+          />
+          <Card image={team4Image} url='https://codepen.io/HRMC/full/RzmpNp' />
+          <Card
+            image={team5Image}
+            url='https://codepen.io/EntranceExit/full/RXgQmr'
+          />
+          <Card
+            image={team6Image}
+            url='https://codepen.io/t_taiki/full/KOMQVO'
+          />
+          <Card image={team7Image} url='https://codepen.io/syuku/full/aevBLR' />
+          <Card image={team8Image} url='' />
           <Card image={vdslabImage} url='https://vdslab.jp' />
           <Card image={judgitImage} url='https://judgit.net' />
           <Card image={wamsimImage} url='https://wamsim.vdslab.jp' />
